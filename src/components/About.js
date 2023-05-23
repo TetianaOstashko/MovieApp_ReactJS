4463e15cd1b67e676814fb5436092a37ae90a371
TetianaOstashko <tetianaostashko.25@gmail.com>
2023-05-24 00:31:58 +0200

    Modal window where you can find the detailed info about the possibilities that this app gives to you

---
+++ src/components/About.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h2>About Movie App</h2>
+      <p>
+        The Movie App is a web application that allows you to discover popular movies and search for your favorite films.
+        You can add movies to your favorites and easily access them later.
+      </p>
+      <p>Key features of the Movie App:</p>
+      <ul>
+        <li>Browse a list of popular movies</li>
+        <li>Search for movies by title</li>
+        <li>Add movies to your favorites</li>
+        <li>View your favorite movies in a separate section</li>
+      </ul>
+    </div>
+  );
+};
+
+export default About;
